159504359f800a828d81fffd619ebf85fe5a5ba2
Kleanta Marianta Sina <37511728+o-mariam@users.noreply.github.com>
2021-01-06 20:20:30 +0200

    master

--- words.py
+++ words.py
@@ -5,6 +5,7 @@ from nltk.corpus import stopwords
 import joblib
 import sqlite3
 from nltk.util import Trie
+import pandas as pd
 
 
 
@@ -20,17 +21,24 @@ conn = sqlite3.connect('./article.db')
 conn.row_factory = sqlite3.Row       
 
 cursor = conn.execute("SELECT article_body FROM article")
+
+
+
+
 filtered_sent=[]
 
 
 for row in cursor:
     article_body= row['article_body']           
     tokenized_word=word_tokenize(article_body)
-    print(nltk.pos_tag(tokenized_word))
+    
   
-    for w in tokenized_word:
-        if w not in stop_words:
-            filtered_sent.append(w)
+for w in tokenized_word:
+    if w not in stop_words:
+        filtered_sent.append(w)
+       
+  
+
     
 
 #print("Tokenized Sentence:",word_tokenize)
